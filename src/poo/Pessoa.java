a5e61cb4fc12e8b89a7bafbca126c69feb2950d4
Andre Felipe de Lara <andre.lara1@alunos.sc.senac.br>
2022-08-29 20:40:15 -0300

    Criacao classe Carro,Pessoa e Barco

---
+++ src/poo/Pessoa.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author Cliente
+ */
+public class Pessoa {
+
+    String nome;
+    String corDoCabelo;
+    String biotipo;
+    int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+
+    }
+
+    public String getCorDoCabelo() {
+        return corDoCabelo;
+    }
+
+    public void setCorDoCabelo(String corDoCabelo) {
+        this.corDoCabelo = corDoCabelo;
+    }
+
+    public String getBiotipo() {
+        return biotipo;
+    }
+
+    public void setBiotipo(String biotipo) {
+        this.biotipo = biotipo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+
+    }
+}
